59f3bef1ddfe4b9d9514390d6645cffbd1481ead
zhuyuanliang <zhuyuanliang@qgutech.com>
2017-11-15 21:21:50 +0800

    spring-boot项目初始化

---
+++ src/main/java/com/springboot/controller/HelloWord.java
@@ -0,0 +1,22 @@
+package com.springboot.controller;
+
+import com.springboot.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by zhu on 2017/7/20.
+ */
+@RestController
+public class HelloWord {
+    @Resource
+    private Student student;
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String sayHello(){
+        return student.getAge();
+    }
+}
